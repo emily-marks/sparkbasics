33fc7f7d50c97f9a09f90b0ef558bd2b64b6b142
emily_marks <emilyhufflish@gmail.com>
2021-08-10 12:26:00 +0300

    geoCode as udf

--- src/main/scala/SparkApp.scala
+++ src/main/scala/SparkApp.scala
@@ -1,9 +1,9 @@
 import ch.hsr.geohash.GeoHash
-import com.opencagedata.geocoder.OpenCageClient
+import com.opencagedata.geocoder.{OpenCageClient, parts}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.functions.{avg, col, udf}
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 import java.util.Locale
 import scala.concurrent.Await
@@ -14,6 +14,7 @@ object SparkApp extends App {
     .master("local[*]")
     .appName("Weather by Hotels Provider")
     .getOrCreate()
+
   val hotelsSchema = ScalaReflection.schemaFor[Hotel].dataType.asInstanceOf[StructType]
   val hotelsDf = sparkSession.read.schema(hotelsSchema).csv("src/main/resources/hotels/part-00000.csv")
   val weatherDf = sparkSession.read.parquet("src/main/resources/weather/part-00229.parquet")
@@ -24,48 +25,43 @@ object SparkApp extends App {
     val longitude = col("longitude")
 
     val geoHashUdf = udf(geoHash _)
+    val geoCodeUdf = udf(geoCode _)
 
-    val correctRows = hotelsDf.filter(latitude.isNotNull && longitude.isNotNull)
-      .withColumn("geohash", geoHashUdf(latitude, longitude))
+    val correctRows =
+      hotelsDf.filter(latitude.isNotNull && longitude.isNotNull)
+        .withColumn("geohash", geoHashUdf(latitude, longitude))
 
-    val incorrectRows = hotelsDf.filter(latitude.isNull || longitude.isNull)
-    val fixedIncorrectRows = incorrectRows.map(row => getGeocode(row))(hotelsDf.encoder)
-      .withColumn("geohash", geoHashUdf(latitude, longitude))
+    val fixedIncorrectRows =
+      hotelsDf.filter(col("id").isNotNull && (latitude.isNull || longitude.isNull))
+        .withColumn("coordinates", geoCodeUdf(col("country"), col("city"), col("address")))
+        .withColumn("latitude", col("coordinates").getField("lat"))
+        .withColumn("longitude", col("coordinates").getField("lng"))
+        .withColumn("geohash", geoHashUdf(latitude, longitude))
+        .drop("coordinates")
 
     val enrichedHotels = correctRows.union(fixedIncorrectRows)
     val enrichedWeather = weatherDf.withColumn("geohash", geoHashUdf(col("lat"), col("lng")))
-    .groupBy("geohash").agg(avg(col("avg_tmpr_f")), avg(col("avg_tmpr_c")))
+      .groupBy("geohash").agg(avg(col("avg_tmpr_f")), avg(col("avg_tmpr_c")))
 
     val join = enrichedHotels.join(enrichedWeather, enrichedWeather("geohash") === enrichedHotels("geohash"), "left")
-//    assert(enrichedHotels.count() == join.count())
-    join.show()
   }
   finally {
     client.close()
     sparkSession.stop()
   }
 
+  def geoCode(country: String, city: String, address: String): Option[parts.LatLong] = {
+    val responseFuture = client.forwardGeocode(buildProperAddress(country, city, address))
+    val response = Await.result(responseFuture, 5.seconds) //todo revise timeout try-catch?
+    response.results.head.geometry
+  }
+
   def geoHash(lat: Double, lng: Double) = {
     GeoHash.geoHashStringWithCharacterPrecision(lat, lng, 4)
   }
 
-  private def getGeocode(row: Row) = {
-    val searchAddress = buildProperAddress(row.getAs("country"), row.getAs("city"), row.getAs("address"))
-    if (!searchAddress.isBlank) {
-      val responseFuture = client.forwardGeocode(searchAddress)
-      val response = Await.result(responseFuture, 5.seconds) //todo revise timeout try-catch?
-      Row(row(0), row(1), row(2), row(3), row(4), response.results.head.geometry.get.lat, response.results.head.geometry.get.lng) //todo is there any other pretty way to make a replacement? probably using dataset api?
-    } else {
-      row
-    }
-  }
-
   def buildProperAddress(country: String, city: String, address: String): String = {
-    if (city.equals("city") && country.equals("country")) {
-      return null
-    }
-
-    if ((address != null) && address.contains(city)) {
+    if (address != null && address.contains(city)) {
       address.replace(city, ", " + city + ",")
     } else {
       city + ", " + new Locale("", country).getDisplayCountry
